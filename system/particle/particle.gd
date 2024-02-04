d3b6f6cff9615d9e2ba74efccbec91ab70ab6368
pixel-boy <pixel-boy@hotmail.fr>
2024-02-04 21:49:49 +0100

    minor change on particle

--- system/particle/particle.gd
+++ system/particle/particle.gd
@@ -3,6 +3,7 @@
 extends GPUParticles2D
 class_name Particle
 
+
 @export var resource_particle:ResourceParticle:
 	set(v):
 		resource_particle = v
@@ -10,5 +11,11 @@ class_name Particle
 			texture = null
 			material = null
 			return
+		if !resource_particle.changed.is_connected(update_particle):
+			resource_particle.changed.connect(update_particle)
+		update_particle()
+
+
+func update_particle():
 		texture = resource_particle.texture
 		material = resource_particle.get_material()
--- system/particle/resource_particle.gd
+++ system/particle/resource_particle.gd
@@ -1,17 +1,35 @@
+@icon("../particle/icon_resource_particle.png")
 @tool
 extends Resource
 class_name ResourceParticle
 
 
-@export var texture:Texture2D
-@export var frame_count := 1
+@export var texture:Texture2D:
+	set(v):
+		texture = v
+		emit_changed()
+@export var frame_h := 1:
+	set(v):
+		frame_h = v
+		generate_material()
+		emit_changed()
+@export var frame_v := 1:
+	set(v):
+		frame_v = v
+		generate_material()
+		emit_changed()
 
 var material:CanvasItemMaterial
 
 
 func get_material()->CanvasItemMaterial:
 	if !material:
-		material = CanvasItemMaterial.new()
-		material.particles_animation = true
-		material.particles_anim_h_frames = frame_count
+		generate_material()
 	return material
+
+
+func generate_material():
+	material = CanvasItemMaterial.new()
+	material.particles_animation = true
+	material.particles_anim_h_frames = frame_h
+	material.particles_anim_v_frames = frame_v
